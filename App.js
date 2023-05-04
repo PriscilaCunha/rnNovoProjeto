b22ada45238dc9f3e4b1863425549d05c51c6e50
PriscilaCunha <priscilacunha@msn.com>
2023-05-04 11:51:55 -0300

    Aula 10.2 - Adicionado elementos

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { ScrollView, Text, TextInput, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <Text>
+        ~{"\n\n\n"}
+        Turma IGTI
+      </Text>
+
+      <View>
+        <Text>Desenvolvedor Mobile</Text>
+
+        <Image
+          source={{
+            uri: "https://cdn.icon-icons.com/icons2/2699/PNG/512/android_official_logo_icon_167880.png"
+          }}
+          style={{ width: 170, height: 200 }}
+        />
+      </View>
+
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        defaultValue='Insira um texto'
+      />
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
